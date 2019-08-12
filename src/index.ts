5adaac43b9419eaa6c72683c70fd3849283c10b3
Federico Angaramo <federico.angaramo@outlook.com>
2019-08-12 22:49:22 +0200

    Fixed ngrok connection

--- src/index.ts
+++ src/index.ts
@@ -220,32 +220,28 @@ const expressServer = expressApp.listen(appPort, () => {
   console.log(`Smart home server listening at ${address}:${port}`)
 
   if (Config.useNgrok) {
-    ngrok.connect(Config.expressPort, (err, url) => {
-      if (err) {
-        console.error('Ngrok was unable to start')
-        console.error(err)
-        process.exit()
-      }
-
-      console.log('')
-      console.log('COPY & PASTE NGROK URL BELOW')
-      console.log(url)
-      console.log('')
-      console.log('=====')
-      console.log('Visit the Actions on Google console at http://console.actions.google.com')
-      console.log('Replace the webhook URL in the Actions section with:')
-      console.log('    ' + url + '/smarthome')
-
-      console.log('')
-      console.log('In the console, set the Authorization URL to:')
-      console.log('    ' + url + '/fakeauth')
-
-      console.log('')
-      console.log('Then set the Token URL to:')
-      console.log('    ' + url + '/faketoken')
-      console.log('')
-
-      console.log('Finally press the \'TEST DRAFT\' button')
-    })
+    (async function() {
+        const url = await ngrok.connect(Config.expressPort)
+        console.log(url)
+        console.log('')
+        console.log('COPY & PASTE NGROK URL BELOW')
+        console.log(url)
+        console.log('')
+        console.log('=====')
+        console.log('Visit the Actions on Google console at http://console.actions.google.com')
+        console.log('Replace the webhook URL in the Actions section with:')
+        console.log('    ' + url + '/smarthome')
+        
+        console.log('')
+        console.log('In the console, set the Authorization URL to:')
+        console.log('    ' + url + '/fakeauth')
+        
+        console.log('')
+        console.log('Then set the Token URL to:')
+        console.log('    ' + url + '/faketoken')
+        console.log('')
+        
+        console.log('Finally press the \'TEST DRAFT\' button')
+    })()
   }
 })
